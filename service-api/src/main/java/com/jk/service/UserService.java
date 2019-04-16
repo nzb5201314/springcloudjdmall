056e5d23b63fd54e3705f886bd82edd1a4b0e672
nzb5201314 <1393996613@qq.com>
2019-04-16 18:40:31 +0800

    前端框架修改

---
+++ service-api/src/main/java/com/jk/service/UserService.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C), 2019
+ * FileName: UserService
+ * Author:   nzb
+ * Date:     2019/4/16 18:34
+ * History:
+ * nzb              <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jk.service;
+
+/**
+ * 〈一句话功能简述〉〈〉
+ *
+ * @author nzb
+ * @create 2019/4/16
+ * @since 1.0.0
+ */
+public class UserService {
+
+}
