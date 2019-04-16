f1297f93d69973d0e7b5555559cd3dcaa77d3533
“ht33 <13186266762@163.com>
2019-04-16 19:36:04 +0800

    郝天 加controller文件

---
+++ springcloud-provider/src/main/java/com/jk/controller/serlogin/UserLogin.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2019,金科
+ * FileName: UserLogin
+ * Author:   HT
+ * Date:     2019/4/16 19:32
+ * History:
+ * HT          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈a〉
+ *
+ * @author Administrator
+ * @create 2019/4/16
+ * @since 1.0.0
+ */
+public class UserLogin {
+
+}
+
