45888e3095776d858490cf5629d19ba4ae83dad2
liangh <1057820252qq.com>
2019-04-18 18:28:39 +0800

    检索页面及后台 for liangh

--- springcloud-feign/src/main/java/com/jk/controller/solr/SolrController.java
+++ springcloud-feign/src/main/java/com/jk/controller/solr/SolrController.java
@@ -7,7 +7,7 @@
  */
 package com.jk.controller.solr;
 
-import com.jk.model.solr.SolrBean;
+import com.jk.model.goods.GoodsBean;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
@@ -40,28 +40,34 @@ public class SolrController {
         @Autowired
         private SolrClient client;
 
+        @RequestMapping("tomian")
+        public  String tomain(){
+                return "solrIndex";
+        }
+
+
         @RequestMapping(value="search")
         @ResponseBody
-        public Map<String,Object> userlist(SolrBean bean, Integer page, Integer rows) throws IOException, SolrServerException {
+        public Map<String,Object> userlist(GoodsBean bean, Integer page, Integer rows) throws IOException, SolrServerException {
                 //因为使用easyui返回数据
                 Map<String,Object> mSolr=new HashMap<String,Object>();
                 //把所有查询的高亮显示内容发到list中
-                List<SolrBean> userslist=new ArrayList<SolrBean>();
+                List<GoodsBean> goodslist=new ArrayList<GoodsBean>();
                 //查询条件的对象
                 SolrQuery params = new SolrQuery();
                 //判断前台传递的关键字是否为空
-                if(!"".equals(bean.getProduct_title()) && bean.getProduct_title()!=null ){
+                if(!"".equals(bean.getGoodsName()) && bean.getGoodsName()!=null ){
                         //不为空设置条件为关键字
-                        params.set("q", bean.getProduct_title());
+                        params.set("q", bean.getGoodsName());
                 }else{//如果为空查询所有
                         params.set("q", "*:*");
                 }
                 //默认查询字段  一般默认指定
-                params.set("df", "product_title");
+                params.set("df", "goodsName");
                 //指定查询结果返回哪些字段
-                params.set("fl", "id,product_title,product_sell_point,product_price,product_num,product_keywords");
+                params.set("fl", "goodsId,goodsName,goodsStatus,goodsPrice,goodsBrand ");
                 // 设置高亮字段
-                params.addHighlightField("product_title"); // 高亮字段
+                params.addHighlightField("goodsName"); // 高亮字段
                 //分页
                 if(page==null){
                         params.setStart(0);
@@ -82,8 +88,8 @@ public class SolrController {
                 params.setHighlightSimplePre("<span style='color:red'>");
                 //设置后缀
                 params.setHighlightSimplePost("</span>");
-                //QueryResponse是查询返回的对象数据   client.query("core1",params)  查询的是哪个索引库和条件
-                QueryResponse queryResponse = client.query("core1",params);
+                //QueryResponse是查询返回的对象数据   client.query("core2",params)  查询的是哪个索引库和条件
+                QueryResponse queryResponse = client.query("core2",params);
                 ///查询返回的结果list对象   不包括高亮
                 SolrDocumentList results = queryResponse.getResults();
                 //查询出来总条数
@@ -93,28 +99,28 @@ public class SolrController {
                 //循环查询的所有结果
                 for (SolrDocument result : results) {
                         //创建对象接收循环的对象数据
-                        SolrBean solrBean=new SolrBean();
+                        GoodsBean goods=new GoodsBean();
                         //设置高亮的字段
                         String highname="";
                         //根据id获得高亮的内容
-                        Map<String, List<String>> map = highlight.get(result.get("id"));
+                        Map<String, List<String>> map = highlight.get(result.get("goodsId"));
                         //根据高亮字段拿到数据
-                        List<String> list = map.get("product_title");
+                        List<String> list = map.get("goodsName");
                         //判断数据是否为空
                         if(list==null){
                                 //如果为空把普通字段放到对象中
-                                highname=(String)result.get("product_title");
+                                highname=(String)result.get("goodsName");
                         }else{
                                 //获得高亮字段查询的值放到变量
                                 highname=list.get(0);
                         }
-                        solrBean.setId((String) result.get("id"));
-                        solrBean.setProduct_sell_point((String)result.get("product_sell_point"));
-                        solrBean.setProduct_title(highname);
-                        userslist.add(solrBean);
+                        goods.setGoodsId((Integer) result.get("goodsId"));
+                        goods.setGoodsName(highname);
+                        goods.setGoodsTypeName(highname);
+                        goodslist.add(goods);
                 }
                 mSolr.put("total",numFound);
-                mSolr.put("rows",userslist);
+                mSolr.put("rows",goodslist);
                 return mSolr;
         }
 
